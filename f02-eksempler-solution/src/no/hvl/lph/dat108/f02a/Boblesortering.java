6a7b2a787459cd25a15dfe9b7c0c57e142e533b5
tfroland <tfroland@gmail.com>
2024-08-21 13:51:30 +0200

    Løsningsforslag for første program i forelesning 2.

--- f02-eksempler-solution/src/no/hvl/lph/dat108/f02a/Boblesortering.java
+++ f02-eksempler-solution/src/no/hvl/lph/dat108/f02a/Boblesortering.java
@@ -19,4 +19,20 @@ public class Boblesortering {
 			}
 		}
 	}
+
+	public static <T extends Comparable<T>> void sorter(List<T> liste, Sammenligner<T> s) {
+		
+		for (int i=0; i<liste.size(); i++) {
+			for (int j=1; j<liste.size(); j++) {
+				T a = liste.get(j-1);
+				T b = liste.get(j);
+				
+				if (s.sammenlign(a, b) > 0) { //Dette er den sentrale linjen
+					
+					liste.set(j-1, b);
+					liste.set(j, a);
+				}
+			}
+		}
+	}
 }
